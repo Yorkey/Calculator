b2d7cd3acecb4d7f9100090b3f21a422904fbadf
wangyu <815776870@qq.com>
2016-10-28 10:31:52 +0800

    添加codepush,添加发布相关

--- android/app/src/main/java/com/calculator/MainApplication.java
+++ android/app/src/main/java/com/calculator/MainApplication.java
@@ -31,7 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG)
+            new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG)
       );
     }
   };
